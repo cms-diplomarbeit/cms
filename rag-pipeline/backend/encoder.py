df1b5216c976b61fc453bc9752527413d1881c8c
gastelsberger <l.gastelsberger@students.htl-leonding.ac.at>
2025-07-07 08:27:10 +0200

    tried to create a rag pipeline

--- rag-pipeline/backend/encoder.py
+++ rag-pipeline/backend/encoder.py
@@ -13,15 +13,3 @@ def encode_documents(model, documents):
     Encodes a list of documents using the provided model
     """
     return model.encode(documents, convert_to_tensor=False)
-
-if __name__ == "__main__":
-    docs = [
-        "RAG kombiniert Retrieval mit Generierung.",
-        "SBERT erzeugt semantische Embeddings.",
-        "Qdrant ist eine Vektordatenbank."
-    ]
-    model = load_encoder()
-    vectors = encode_documents(model, docs)
-
-    for i, vec in enumerate(vectors):
-        print(f"Vektor für Dokument {i}: {vec[:5]}... ({len(vec)} Dimensionen)")
